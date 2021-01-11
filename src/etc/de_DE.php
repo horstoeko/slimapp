b7779c720059e9f0998d740145d0683698154805
ruff <ruff@thor>
2021-01-11 01:45:45 +0100

    Fix translation of the validator

---
+++ src/etc/de_DE.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    // Validation Translations
+    "{{name}} must not contain whitespace" => "{{name}} darf keine Leerzeichen enthalten",
+    "{{name}} must not be empty" => "{{name}} darf nicht leer sein",
+    "{{name}} must be greater than or equal to {{interval}}" => "{{name}} muss größer oder gleich {{interval}} sein",
+    "{{name}} must be positive" => "{{name}} muss positiv sein",
+    "{{name}} must be numeric" => "{{name}} must be numeric",
+    "{{name}} must be a valid date. Sample format: {{format}}" => "{{name}} muss ein gültiges Datum in der Form {{format}} sein",
+    "At least one of these rules must pass for {{name}}" => "Mindestens eine dieser Regeln muss für {{name}} gelten.",
+    "{{name}} must be valid email" => "{{name}} muss eine gültige E-Mailadresse sein",
+    "{{name}} is already taken" => "{{name}} wird bereits verwendet",
+    "{{name}} must have a length greater than {{minValue}}" => "{{name}} muss eine Länge von mehr als {{minValue}} Zeichen haben",
+    "{{name}} must have a length lower than {{maxValue}}" => "{{name}} muss eine Länge kleiner sein als {{maxValue}} Zeichen haben",
+    "{{name}} must have a length between {{minValue}} and {{maxValue}}" => "{{name}} muss eine Länge zwischen {{minValue}} und {{maxValue}} haben",
+    "{{name}} must be between {{minAge}} and {{maxAge}} years ago" => "{{name}} muss vor {{minAge}} und {{maxAge}} Jahren liegen",
+    "{{name}} must be lower than {{minAge}} years ago" => "{{name}} muss niedriger sein als vor {{minAge}} Jahren",
+    "{{name}} must be greater than {{maxAge}} years ago" => "{{name}} muss größer sein als vor {{maxAge}} Jahren",
+    "{{name}} must be less than {{interval}}" => "{{name}} muss kleiner als {{interval}} sein",
+    "{{name}} must be less than or equal to {{interval}}" => "{{name}} muss kleiner oder gleich {{interval}} sein",
+    "{{name}} must be numeric" => "{{name}} muss numerisch sein",
+];
